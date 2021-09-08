de7c813a9472d9922e774d191de7f42721e6b0fb
massimo.daros <massimo.daros@external.cerved.com>
2021-09-08 16:24:42 +0200

    ModelConverter refactoring

--- src/ModelConverter.ts
+++ src/ModelConverter.ts
@@ -5,7 +5,6 @@ import { ImageWidget } from "./model/ImageWidget";
 import { ImageWidgetEntity } from "./entity/ImageWidgetEntity";
 import { Widget } from "./model/Widget";
 
-// TODO Refactor to avoid code duplication
 export class ModelConverter {
 
     public toTextWidgetEntity = ( widget: TextWidget ): TextWidgetEntity => {
@@ -23,63 +22,37 @@ export class ModelConverter {
         var entity = new ImageWidgetEntity ();
         entity.id     = widget.id;
         entity.src    = widget.src;
-        entity.widget = this.toEntity ( widget as unknown as Widget );;
+        entity.widget = this.toEntity ( widget as unknown as Widget );
 
         return entity;
     }
 
-    public toTextWidgetEntities = ( textWidgets: TextWidget [] ): TextWidgetEntity [] => {
-
-        return textWidgets
-            .map ( textWidget => this.toTextWidgetEntity ( textWidget ) );
-    }
-
-    public toImageWidgetEntities = ( imageWidgets: ImageWidget [] ): ImageWidgetEntity [] => {
-
-        return imageWidgets
-            .map ( imageWidget => this.toImageWidgetEntity ( imageWidget ) );
-    }
-
     public toTextWidget = ( entity: TextWidgetEntity ): TextWidget => {
 
-        var widget = new TextWidget ();
-        widget.id       = entity.id;
-        widget.name     = entity.widget.name;
-        widget.type     = entity.widget.type;
-        widget.x        = entity.widget.x;
-        widget.y        = entity.widget.y
-        widget.z        = entity.widget.z;
-        widget.width    = entity.widget.width;
-        widget.height   = entity.widget.height;
-        widget.selected = entity.widget.selected;
-        widget.start    = entity.widget.start;
-        widget.end      = entity.widget.end;
-        widget.visible  = entity.widget.visible;
-        widget.locked   = entity.widget.locked;
-        widget.text     = entity.text;
+        var textWidget = { ...entity.widget } as unknown as TextWidget;
+        textWidget.id   = entity.id;
+        textWidget.text = entity.text;
 
-        return widget;
+        return textWidget;
     }
 
     public toImageWidget = ( entity: ImageWidgetEntity ): ImageWidget => {
 
-        var widget = new ImageWidget ();
-        widget.id       = entity.id;
-        widget.name     = entity.widget.name;
-        widget.type     = entity.widget.type;
-        widget.x        = entity.widget.x;
-        widget.y        = entity.widget.y
-        widget.z        = entity.widget.z;
-        widget.width    = entity.widget.width;
-        widget.height   = entity.widget.height;
-        widget.selected = entity.widget.selected;
-        widget.start    = entity.widget.start;
-        widget.end      = entity.widget.end;
-        widget.visible  = entity.widget.visible;
-        widget.locked   = entity.widget.locked;
-        widget.src      = entity.src;
+        var textWidget = { ...entity.widget } as unknown as ImageWidget;
+        textWidget.id  = entity.id;
+        textWidget.src = entity.src;
 
-        return widget;
+        return textWidget;
+    }
+
+    public toWidget = ( entity: WidgetEntity ): Widget => {
+
+        return { ...entity } as Widget;
+    }
+
+    public toEntity = ( widget: WidgetEntity ): WidgetEntity => {
+
+        return { ...widget } as WidgetEntity;
     }
 
     public toTextWidgets = ( entities: TextWidgetEntity [] ): TextWidget [] => {
@@ -94,44 +67,16 @@ export class ModelConverter {
             .map ( imageWidget => this.toImageWidget ( imageWidget ) );
     }
 
-    public toWidget = ( entity: WidgetEntity ): Widget => {
+    public toTextWidgetEntities = ( textWidgets: TextWidget [] ): TextWidgetEntity [] => {
 
-        var widget = new Widget ();
-        widget.id       = entity.id;
-        widget.name     = entity.name;
-        widget.type     = entity.type;
-        widget.x        = entity.x;
-        widget.y        = entity.y;
-        widget.z        = entity.z;
-        widget.width    = entity.width;
-        widget.height   = entity.height;
-        widget.selected = entity.selected;
-        widget.start    = entity.start;
-        widget.end      = entity.end;
-        widget.visible  = entity.visible;
-        widget.locked   = entity.locked;
-
-        return widget;
+        return textWidgets
+            .map ( textWidget => this.toTextWidgetEntity ( textWidget ) );
     }
 
-    public toEntity = ( widget: WidgetEntity ): WidgetEntity => {
+    public toImageWidgetEntities = ( imageWidgets: ImageWidget [] ): ImageWidgetEntity [] => {
 
-        var entity = new WidgetEntity ();
-        entity.id       = widget.id;
-        entity.name     = widget.name;
-        entity.type     = widget.type;
-        entity.x        = widget.x;
-        entity.y        = widget.y;
-        entity.z        = widget.z;
-        entity.width    = widget.width;
-        entity.height   = widget.height;
-        entity.selected = widget.selected;
-        entity.start    = widget.start;
-        entity.end      = widget.end;
-        entity.visible  = widget.visible;
-        entity.locked   = widget.locked;
-
-        return entity;
+        return imageWidgets
+            .map ( imageWidget => this.toImageWidgetEntity ( imageWidget ) );
     }
 
     public toWidgets = ( entities: WidgetEntity [] ): Widget [] => {
